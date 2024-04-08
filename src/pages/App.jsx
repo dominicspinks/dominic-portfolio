cfe865693c9a2b128d4cd41432d6299c958d0678
Dominic Spinks <dominic.spinks@outlook.com>
2024-04-09 07:55:54 +1000

    Add placeholder landing page

---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<>
+			<h1>Dominic Spinks</h1>
+			<p>Portfolio coming soon...</p>
+			<a href='https://github.com/dominicspinks' target='_blank'>
+				Please visit my GitHub profile instead
+			</a>
+		</>
+	);
+}
+
+export default App;
