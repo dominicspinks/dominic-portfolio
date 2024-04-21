86bbfc58dec6c8a17fb5404fbbadf30af9af329a
Dominic Spinks <dominic.spinks@outlook.com>
2024-04-22 08:42:52 +1000

    Update project section; Add contact form

---
+++ src/components/Contact.jsx
@@ -0,0 +1,94 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export default function Contact() {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+		console.log(form);
+		emailjs
+			.sendForm('service_9ninlqb', 'template_3i22kjj', form.current, {
+				publicKey: '_vScShfGw_s20VTwu',
+			})
+			.then(
+				() => {
+					console.log('SUCCESS!');
+				},
+				(error) => {
+					console.log('FAILED...', error.text);
+				}
+			);
+	};
+
+	return (
+		<div
+			id='contact'
+			className='max-w-[1000px] m-auto p-4 md:pl-20 p-4 py-16'>
+			<h1 className='py-4 text-4xl font-bold text-center text-slate-700'>
+				Contact
+			</h1>
+			<form ref={form} onSubmit={sendEmail} className=''>
+				<div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+					<div className='flex flex-col'>
+						<label
+							htmlFor='from_name'
+							className='uppercase text-sm py-2'>
+							Name *
+						</label>
+						<input
+							type='text'
+							id='from_name'
+							name='from_name'
+							className='border-2 border-gray-300 rounded-lg p-3 flex border-gray-300'
+							required
+						/>
+					</div>
+					<div className='flex flex-col'>
+						<label
+							htmlFor='from_phone'
+							className='uppercase text-sm py-2'>
+							Phone Number
+						</label>
+						<input
+							type='text'
+							id='from_phone'
+							name='from_phone'
+							className='border-2 border-gray-300 rounded-lg p-3 flex border-gray-300'
+						/>
+					</div>
+				</div>
+				<div className='flex flex-col'>
+					<label
+						htmlFor='from_email'
+						className='uppercase text-sm py-2'>
+						Email *
+					</label>
+					<input
+						type='email'
+						id='from_email'
+						name='from_email'
+						className='border-2 border-gray-300 rounded-lg p-3 flex border-gray-300'
+						required
+					/>
+				</div>
+				<div className='flex flex-col'>
+					<label htmlFor='message' className='uppercase text-sm py-2'>
+						Message *
+					</label>
+					<textarea
+						rows='10'
+						id='message'
+						name='message'
+						className='border-2 border-gray-300 rounded-lg p-3 flex border-gray-300'
+						required></textarea>
+				</div>
+				<button
+					type='submit'
+					className='bg-slate-900 py-3 my-8 px-8 rounded-lg text-white font-bold'>
+					Send Message
+				</button>
+			</form>
+		</div>
+	);
+}
--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,10 +1,18 @@
 export default function ProjectItem({ img, title }) {
-    return (
-        <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-            <img src={img} alt={title} />
-            <div>
-                <h3>{title}</h3>
-            </div>
-        </div>
-    );
+	return (
+		<div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+			<img src={img} alt={title} />
+			<div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+				<h3 className='text-2xl text-white tracking-wider font-bold text-center'>
+					{title}
+				</h3>
+				<p className='pb-4 pt-2 text-white text-center'>React</p>
+				<a href='/'>
+					<p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>
+						More Info
+					</p>
+				</a>
+			</div>
+		</div>
+	);
 }
