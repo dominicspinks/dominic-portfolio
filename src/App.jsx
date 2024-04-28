9ee456e2834638a33ee186a21533a766597349b2
Dominic Spinks <dominic.spinks@outlook.com>
2024-04-28 14:51:44 +1000

    update main section; Add resume link; Add about me section

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import Sidenav from './components/Sidenav';
 import Main from './components/Main';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import AboutMe from './components/AboutMe';
 
 function App() {
-	return (
-		<>
-			<Sidenav />
-			<Main />
-			<Projects />
-			<Contact />
-		</>
-	);
+    return (
+        <>
+            <Sidenav />
+            <Main />
+            <AboutMe />
+            <Projects />
+            <Contact />
+        </>
+    );
 }
 
 export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,50 +1,61 @@
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { HiMail } from 'react-icons/hi';
 
-import backgroundImg from '../assets/background.jpg';
+import profileImg from '../assets/profile.png';
+import resumeFile from '/DominicSpinksResume.pdf';
 
 export default function Main() {
-	return (
-		<div id='main'>
-			<img
-				src={backgroundImg}
-				alt='background'
-				className='w-full h-screen object-cover object-left'
-			/>
-			<div className='w-full h-screen absolute top-0 left-0 bg-white opacity-60'>
-				<div className='max-w-[700px] m-auto w-full h-full flex flex-col justify-center lg:items-start items-center'>
-					<h1 className='sm:text-5xl text-3xl font-bold text-slate-900'>
-						I'm Dominic Spinks
-					</h1>
-					<h3 className='sm:text-3xl text-2xl pt-4 text-gray-800'>
-						Software Engineer
-					</h3>
-					<div className='flex justify-between pt-6 max-w-[150px] w-full'>
-						<a
-							href='https://github.com/dominicspinks'
-							target='_blank'>
-							<FaGithub
-								className='hover:scale-110 ease-in duration-300 cursor-pointer'
-								size={25}
-							/>
-						</a>
-						<a
-							href='https://www.linkedin.com/in/dominic-spinks/'
-							target='_blank'>
-							<FaLinkedinIn
-								className='hover:scale-110 ease-in duration-300 cursor-pointer'
-								size={25}
-							/>
-						</a>
-						<a href='mailto:contact@dominicspinks.com'>
-							<HiMail
-								className='hover:scale-110 ease-in duration-300 cursor-pointer'
-								size={25}
-							/>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div id='main'>
+            <div className='w-full h-screen top-0 left-0 bg-white flex flex-col lg:flex-row justify-center items-center gap-6'>
+                <img
+                    src={profileImg}
+                    alt='background'
+                    className='max-w-[250px] border-4 border-slate-900 rounded-full'
+                />
+                <div className='max-w-[700px] flex justify-center items-center gap-4'>
+                    <div className='flex flex-col justify-center items-start'>
+                        <h1 className='sm:text-5xl text-3xl font-bold text-slate-900'>
+                            Hi, I'm Dominic!
+                        </h1>
+                        <h3 className='sm:text-3xl text-2xl pt-4 text-gray-800'>
+                            Software Engineer
+                            <br />
+                            Based in Melbourne
+                        </h3>
+                        <a
+                            href={resumeFile}
+                            target='_blank'
+                            className='border-2 border-slate-900 px-4 py-2 w-full text-center mt-4 hover:border-slate-700 hover:bg-slate-500 hover:text-white'>
+                            Check out my Resume Here
+                        </a>
+                    </div>
+                    <div className='flex flex-col justify-around pl-4 gap-4'>
+                        <a
+                            href='https://github.com/dominicspinks'
+                            target='_blank'>
+                            <FaGithub
+                                className='hover:scale-110 ease-in duration-300 cursor-pointer'
+                                size={30}
+                            />
+                        </a>
+                        <a
+                            href='https://www.linkedin.com/in/dominic-spinks/'
+                            target='_blank'>
+                            <FaLinkedinIn
+                                className='hover:scale-110 ease-in duration-300 cursor-pointer'
+                                size={30}
+                            />
+                        </a>
+                        <a href='mailto:contact@dominicspinks.com'>
+                            <HiMail
+                                className='hover:scale-110 ease-in duration-300 cursor-pointer'
+                                size={30}
+                            />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
