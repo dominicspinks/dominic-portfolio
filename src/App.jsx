7bba2dd5c9d3844313fe393ae14bb4df03a8d86f
Dominic Spinks <dominic.spinks@outlook.com>
2024-05-19 13:43:35 +1000

    Add footer compontent

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Main from './components/Main';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import AboutMe from './components/AboutMe';
+import Footer from './components/Footer';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <AboutMe />
             <Projects />
             <Contact />
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer
+            id='footer'
+            className='w-full h-16 flex justify-center items-center border-t border-slate-900'>
+            <p className='text-slate-900'>© 2024 Dominic Spinks</p>
+        </footer>
+    );
+}
